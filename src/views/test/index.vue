f135a089e87073581e70add10f84c4ed7b32fdf3
hedahang <hdh_angular@163.com>
2021-11-30 10:34:06 +0800

    feat:页面缓存状态设置

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,3 +1,29 @@
 <template>
   <div class="test">13131231</div>
 </template>
+<script lang="ts">
+export default {
+  name: "testIndex"
+};
+</script>
+<script setup lang="ts">
+import { onBeforeMount, onActivated, onDeactivated } from "vue";
+console.log("create");
+onBeforeMount(() => {
+  console.log(12312);
+});
+onActivated(() => {
+  console.log("onActivated");
+});
+onDeactivated(() => {
+  console.log("onDeactivated");
+});
+</script>
+
+<style lang="scss" scoped>
+.test {
+  width: 200px;
+  height: 200px;
+  background-color: aquamarine;
+}
+</style>
